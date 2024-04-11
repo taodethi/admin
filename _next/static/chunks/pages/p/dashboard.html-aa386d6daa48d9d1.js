47109e6f38eae66a710b894aed15981fb8c63666
taodethi <87457500+taodethi@users.noreply.github.com>
2024-04-12 00:18:06 +0700

    Add files via upload

---
+++ _next/static/chunks/pages/p/dashboard.html-aa386d6daa48d9d1.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[602],{6244:function(n,u,t){(window.__NEXT_P=window.__NEXT_P||[]).push(["/p/dashboard.html",function(){return t(9664)}])},9664:function(n,u,t){"use strict";t.r(u);var _=t(5893);u.default=()=>(0,_.jsx)(_.Fragment,{children:"Dashboard"})}},function(n){n.O(0,[774,888,179],function(){return n(n.s=6244)}),_N_E=n.O()}]);
